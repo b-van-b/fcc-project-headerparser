7d22fff6505de1e94e238bb86c4ce98c554bbdab
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-19 18:07:26 +0000

    feat: add whoami api\nchore: prettify

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+// return user ip, language, and browser software from request header
+app.get("/api/whoami", function (req, res) {
+  console.log("\n\n" + JSON.stringify(req.headers));
+  res.json({
+    ipaddress: req.headers["x-forwarded-for"],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
 });
 
 // listen for requests :)
